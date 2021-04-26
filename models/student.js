06dff78297751e3eb411d481e612c0a0564c3430
rromer07 <69991220+rromer07@users.noreply.github.com>
2021-04-26 11:15:56 -0400

    Add files via upload

---
+++ models/student.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const Schema = mongoose.Schema;
+
+const studentSchema = new Schema({
+    name: {
+        type:String,
+        required:false
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        validate:(value)=>{return validator.isEmail(value)}
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    totalGrade:{
+        type:Number,
+        required:true,
+        default:0
+    },
+},  {timestamps:true});
+
+const Student = mongoose.model('Student',studentSchema);
+module.exports = Student;
---
+++ models/teacher.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const Schema = mongoose.Schema;
+
+const teacherSchema = new Schema({
+    name: {
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        validate:(value)=>{return validator.isEmail(value)}
+    },
+    password:{
+        type:String,
+        required:true
+    },
+},  {timestamps:true});
+
+const Teacher = mongoose.model('Teacher',teacherSchema);
+module.exports = Teacher;
