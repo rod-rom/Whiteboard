e687081284bf23df1a680b12bdf80c14073261c7
rromer07 <69991220+rromer07@users.noreply.github.com>
2021-05-02 00:12:08 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Student = require('./models/student');
-const Teacher = require('./models/teacher');
-const Assignment = require('./models/assignment');
+const studentRoutes = require('./routes/studentRoutes');
+const teacherRoutes = require('./routes/teacherRoutes');
+const assignmentRoutes = require('./routes/assignmentRoutes');
 const mongoClient = require('mongodb').MongoClient;
 
 
@@ -13,63 +13,19 @@ app.use(express.json);
 app.use(express.urlencoded({extended:true}));
 
 //connect to mongodb
-const url = "mongodb://127.0.0.1:27017";
+const url = "mongodb://127.0.0.1:27017/whiteboard";
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true})
-    .then((result)=>app.listen(3000,()=>{console.log('Connected to MongoDB Server, running on port 3000')}))
+    .then((result)=>app.listen(3000,()=>{console.log('Connected to MongoDB Server, running on port 3000');console.log(mongoose.connection.readyState);}))
     .catch((err)=>console.log(err));
 
+//If it returns 1 that means you're connected. If it returns 0 that means you're not connected.
+
 //routes
-app.post('/student/register',(req,res)=>{
-    const student = new Student(req.body);
-    student.save()
-        .then((result)=>{res.status(200).send()})
-        .catch((err)=>{res.status(400).send()})
-   
-});
+app.use('/students',studentRoutes);
+app.use('/teacher',teacherRoutes)
+app.use('/assignment',assignmentRoutes);
 
-app.post('/student/login',(req,res)=>{
-    const student = new Student(req.body);
-    student.equals()
-        .then((result)=>{res.status(200).send();})
-        .catch((err)=>{res.status(400).send()})
-});
-app.post('/teacher/register',(req,res)=>{
-    const teacher = new Teacher(req.body);
-    teacher.save()
-        .then((result)=>{res.status(200).send()})
-        .catch((err)=>{res.status(400).send()})
-   
-});
 
-app.post('/teacher/login',(req,res)=>{
-    const teacher = new Teacher(req.body);
-    teacher.equals()
-        .then((result)=>{res.status(200).send()})
-        .catch((err)=>{res.status(400).send()})
-    
-});
 
-app.get('assignments',(req,res)=>{
-    const assignment = new Assignment();
-    assignment.find()
-        .then((result)=>res.send(result))
-        .catch((err)=>res.status(400).send())
-});
 
-app.post('assignments/:name',(req,res)=>{
-    const assignment = new Assignment (req.param.name);
-    assignment.save()
-        .then((result)=>res.status(200).send())
-        .catch((err)=>res.status(400).send())
-});
 
-app.delete('assignments/:name',(req,res)=>{
-    const assignment = new Assignment(req.params.name);
-    Assignment.findOneAndDelete(assignment)
-        .then((result)=>{
-            res.status(200).send()
-        })
-        .catch((err)=>{
-            res.status(400).send("Error removing assignment")
-        })
-});
