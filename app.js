72df7d0b5309f492d373d276418512051172c80d
rromer07 <69991220+rromer07@users.noreply.github.com>
2021-05-06 15:12:36 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const studentRoutes = require('./routes/studentRoutes');
 const teacherRoutes = require('./routes/teacherRoutes');
 const assignmentRoutes = require('./routes/assignmentRoutes');
-const mongoClient = require('mongodb').MongoClient;
 
 
 //express services
 const app = express();
-app.use(express.json);
 app.use(express.urlencoded({extended:true}));
 
 //connect to mongodb
-const url = "mongodb://127.0.0.1:27017/whiteboard";
+const url = "mongodb+srv://admin:123@cluster0.tdan9.mongodb.net/whiteboard?retryWrites=true&w=majority";
 mongoose.connect(url,{useNewUrlParser:true,useUnifiedTopology: true})
-    .then((result)=>app.listen(3000,()=>{console.log('Connected to MongoDB Server, running on port 3000');console.log(mongoose.connection.readyState);}))
+    .then((result)=>app.listen(2000,()=>{console.log('Connected to MongoDB Server, running on port 2000');console.log(mongoose.connection.readyState);}))
     .catch((err)=>console.log(err));
 
 //If it returns 1 that means you're connected. If it returns 0 that means you're not connected.
 
 //routes
-app.use('/students',studentRoutes);
-app.use('/teacher',teacherRoutes)
+app.use('/student',studentRoutes);
+app.use('/teacher',teacherRoutes);
 app.use('/assignment',assignmentRoutes);
 
 
