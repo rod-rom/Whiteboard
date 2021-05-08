77e597eb46a5a1ae0aae9c5947b1e7b8032e0eb2
rromer07 <69991220+rromer07@users.noreply.github.com>
2021-05-08 18:37:34 -0400

    Add files via upload

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const studentController = require('../controllers/studentController');
 
+router.get('/',studentController.student_get);
 router.post('/login',studentController.student_login);
 router.post('/test',studentController.student_test);
-
 module.exports = router;
